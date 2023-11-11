5fddd4f4ac806dbd700b84cb61f428817a5ddb64
Mazidul_Islam24x7 <121252613+MazidulIslamNeal@users.noreply.github.com>
2023-11-11 18:40:39 +0200

    Create 9.3.py

---
+++ Module-09/9.3.py
@@ -0,0 +1,31 @@
+class Car:
+    def __init__(self, registration_number, max_speed):
+        self.registration_number = registration_number
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate(self, change):
+        new_speed = self.current_speed + change
+        self.current_speed = max(0, min(new_speed, self.max_speed))
+
+    def drive(self, hours):
+        self.travelled_distance += self.current_speed * hours
+
+# Creating a new car
+new_car = Car("ABC-123", 142)
+
+# Accelerating the car
+speed_changes = [30, 70, 50]
+
+for change in speed_changes:
+    new_car.accelerate(change)
+
+# Printing out the current speed of the car
+print(f"Current Speed: {new_car.current_speed} km/h")
+
+# Driving the car for 1.5 hours
+new_car.drive(1.5)
+
+# Printing out the travelled distance
+print(f"Travelled Distance: {new_car.travelled_distance} km")
