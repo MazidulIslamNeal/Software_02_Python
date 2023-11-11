c5c8bb0b3d53b72b49a758050e764c47c9756fc2
Mazidul_Islam24x7 <121252613+MazidulIslamNeal@users.noreply.github.com>
2023-11-11 17:28:31 +0200

    Create 13.1.py

---
+++ Module-13/13.1.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, Response
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/prime_number/<number>')
+def prime_number(number):
+    isPrime = True
+    number = int(number)
+    if number > 1:
+        for i in range(2, number):
+            if (number % i) == 0:
+                isPrime = False
+                break
+
+    response = {
+        "isPrime": isPrime,
+        "Number": number
+    }
+    return response
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
